fc09ab39843050e1f72deeca181b8bd901ee0286
Peter Fessel <peter.fessel@live.de>
2016-09-17 12:38:33 +0100

    generate new revolving circles whenever mouse is pressed

--- src/quil_experiments/core.clj
+++ src/quil_experiments/core.clj
@@ -11,13 +11,18 @@
   ; circle color and position.
   {:color-1 0
    :color-2 255
-   :angle 0})
+   :angle 0
+   :circle-origins [] ; a vector of circle origins {x: 0, y: 0} to save where the user has clicked
+   })
 
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
-  {:color-1 (mod (+ (:color-1 state) 0.7) 255)
+  (let [last-clicked-position (last (:circle-origins state))
+        mouse-pressed (and (q/mouse-pressed?) (and (not= (q/mouse-x) (:x last-clicked-position)) (not= (q/mouse-y) (:y last-clicked-position))))]
+    {:color-1 (mod (+ (:color-1 state) 0.7) 255)
    :color-2 (if (< (- (:color-2 state) 0.7) 0) 255 (- (:color-2 state) 0.7))
-   :angle (+ (:angle state) 0.1)})
+   :angle (+ (:angle state) 0.1)
+   :circle-origins (if mouse-pressed (conj (:circle-origins state) {:x (q/mouse-x) :y (q/mouse-y)}) (:circle-origins state))}))
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
@@ -27,13 +32,22 @@
         x-1 (* 150 (q/cos angle))
         y-1 (* 150 (q/sin angle))
         x-2 (* 100 (q/sin angle))
-        y-2 (* 100 (q/cos angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(q/mouse-x) (q/mouse-y)]
+        y-2 (* 100 (q/cos angle))
+        circle-origins (:circle-origins state)]
+
+      ;; print circles around current mouse position
       (q/fill (:color-1 state) 255 255)
-      (q/ellipse x-2 y-2 50 50)
+      (q/ellipse (+ x-2 (q/mouse-x)) (+ y-2 (q/mouse-y)) 50 50)
       (q/fill (:color-2 state) 255 255)
-      (q/ellipse x-1 y-1 100 100))))
+      (q/ellipse (+ x-1 (q/mouse-x)) (+ y-1 (q/mouse-y)) 100 100)
+
+      ;; print circles around previously clicked mouse positions
+      (doseq [coordinates circle-origins]
+        (do
+          (q/fill (:color-1 state) 255 255)
+          (q/ellipse (+ x-2 (:x coordinates)) (+ y-2 (:y coordinates)) 50 50)
+          (q/fill (:color-2 state) 255 255)
+          (q/ellipse (+ x-1 (:x coordinates)) (+ y-1 (:y coordinates)) 100 100)))))
 
 (q/defsketch quil-experiments
   :title "You spin my circle right round"
