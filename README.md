70c0d17a01f3e7eb376c2c27e855ed1a98070d9f
Peter Fessel <peter.fessel@live.de>
2016-09-17 11:42:30 +0100

    inital commit: simple quil example

---
+++ README.md
@@ -0,0 +1,11 @@
+# quil-experiments
+
+Using Quil to generate some cool graphics.
+
+## Usage
+
+Start the repl with `lein repl`, then run the animation by executing `(use 'quil-experiments.core)`.
+
+## License
+
+Copyright © 2016 Peter Fessel
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject quil-experiments "0.1.0-SNAPSHOT"
+  :description "Using Quil to generate some cool graphics."
+  :url "https://github.com/lepetere/quil-experiments"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [quil "2.4.0"]])
